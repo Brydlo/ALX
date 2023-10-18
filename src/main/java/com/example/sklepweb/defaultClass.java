c6328889e89c8012ad391533b39c8cfaa928569d
Tomasz Czerniewski <brydlo2.tc@gmail.com>
2023-10-18 11:07:20 +0200

    new class

--- src/main/java/com/example/sklepweb/defaultClass.java
+++ src/main/java/com/example/sklepweb/defaultClass.java
@@ -5,7 +5,7 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet {
+public class defaultClass extends HttpServlet {
     private String message;
 
     public void init() {
